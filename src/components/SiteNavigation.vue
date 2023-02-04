301d96a1ca750b5e476297918b28f6cbc85f43b8
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-02-04 11:56:25 +0300

    modal active

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -10,17 +10,35 @@
 
            <div class="flex gap-3 flex-1 justify-end">
             <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-            <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+            <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
            </div> 
 
-           <BaseModal>
-             <h1 class="text-black">Hello Modal</h1>
+           <BaseModal :modalActive="modalActive">
+             <div class="text-black">
+                <h1 class="text-2xl mb-1">About:</h1>
+                <p class="mb-4">The Local Weather allows you to track the current and future wether of cities of your chhosing</p>
+                <h2 class="text-2xl">How it works:</h2>
+                <ol class="list-decimal list-inside mb-4">
+                    <li>Search for your city by entering the nema into the search bar.</li>
+                    <li>Search for your city by entering the nema into the search bar.</li>
+                    <li>Search for your city by entering the nema into the search bar.</li>
+                </ol>
+
+                <h2 class="text-2xl">Removing a city</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat ab nulla nihil explicabo ad aspernatur? Molestias iusto saepe hic provident. Voluptatem quasi eos delectus dolorum.</p>
+             </div>
            </BaseModal>
         </div>
     </header>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { RouterLink } from 'vue-router';
 import BaseModal from "./BaseModal.vue";
+
+const modalActive = ref(null);
+const toggleModal = () => {
+    modalActive.value = !modalActive.value;
+};
 </script>
