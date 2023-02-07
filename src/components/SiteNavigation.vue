f9b1f49da804854f38469e6eef06d8f40594347e
otabekabdusalomov <otabekabdusalamov2001@gmail.com>
2023-02-07 10:54:13 +0300

    search

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -9,11 +9,11 @@
             </RouterLink>
 
            <div class="flex gap-3 flex-1 justify-end">
-            <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-            <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
+            <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
+            <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
            </div> 
 
-           <BaseModal :modalActive="modalActive">
+           <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
              <div class="text-black">
                 <h1 class="text-2xl mb-1">About:</h1>
                 <p class="mb-4">The Local Weather allows you to track the current and future wether of cities of your chhosing</p>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="main">
-    
-  </div>
+  <main class="container text-white">
+    <div></div>
+  </main>
 </template>
 
 <script>
